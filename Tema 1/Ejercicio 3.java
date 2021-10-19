4887cf05bec2b6a55e731b1fd2e1c25cc895884b
Ana Belén Gil <anabelen.gil.fp@iescampanillas.com>
2021-10-19 22:19:44 +0200

    Create Ejercicio 3.java

---
+++ Tema 1/Ejercicio 3.java
@@ -0,0 +1,20 @@
+
+/* Escribe un programa que muestre 10 palabras en 
+ingles junto su traduccion en español. */
+
+public class Ejercicio3 {
+  
+  public static void main (String[] args) {
+    System.out.printf("%-10s %s\n", "computer", "ordenador");
+    System.out.printf("%-10s %s\n", "student", "alumno");
+    System.out.printf("%-10s %s\n", "cat", "gato");
+    System.out.printf("%-10s %s\n", "lion", "leon");
+    System.out.printf("%-10s %s\n", "machine", "maquina");
+    System.out.printf("%-10s %s\n", "nature", "naturaleza");
+    System.out.printf("%-10s %s\n", "light", "luz");
+    System.out.printf("%-10s %s\n", "green", "verde");
+    System.out.printf("%-10s %s\n", "book", "libro");
+    System.out.printf("%-10s %s\n", "pyramid", "piramide");
+  }
+}
+
