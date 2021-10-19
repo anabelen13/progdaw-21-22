a78526b820787bfa9dae01952a17be600e3a684f
Ana Belén Gil <anabelen.gil.fp@iescampanillas.com>
2021-10-19 22:19:40 +0200

    Create Ejercicio 2.java

---
+++ Tema 1/Ejercicio 2.java
@@ -0,0 +1,10 @@
+/ * Modifica el programa anterior para que se muestre tu dirección y
+tu número de teléfono. * /
+
+ public class Ejercicio2 {
+  public  static  void  main ( String [] args ) {
+    Sistema . fuera . println ( " Ana Belén Gil Cortés " );
+    Sistema . fuera . println ( " C / No se Nº4 " );
+    Sistema . fuera . println ( " 629468532 " );
+  }
+}
